b20a702083e0f2eb059f1d3330ce270f53df2fe5
pigmypuff <ienanayakkara07@gmail.com>
2023-01-06 15:50:23 +0530

    created package, cart ui

--- packages.php
+++ packages.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
+  
 
 </head>
 
@@ -54,7 +54,7 @@
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Wedding Package 01</h1>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -87,17 +87,17 @@
                 <p class="card-text">
                  
                 </p>
-                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal1">
+                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal2">
                     See More
                   </button>
               </div>
             </div>
           </div>
-          <div class="modal fade" id="myModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade" id="myModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Wedding Package 02</h1>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -106,7 +106,145 @@
                     <li class="list-group-item">Full function coverage</li>
                     <li class="list-group-item">20' x 30' enlargement with frame</li>
                     <li class="list-group-item">100 thank you cards</li>
-                    <li class="list-group-item fw-semibold">LKR 65 000</li>
+                    <li class="list-group-item">12" x 30" Fine art or Magazine type photo album</li>
+                    <li class="list-group-item fw-semibold">LKR 95 000</li>
+                  </ul>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <a href = "payment.php?id=2">
+                  <button type="button" class="btn btn-outline-dark">Get this package</button></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="gallery2/10.jpg" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Wedding Package 03</h5>
+                <p class="card-text">
+                  
+                </p>
+                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal3">
+                    See More
+                  </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal fade" id="myModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Wedding Package 03</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Photo session with couple on desired location</li>
+                    <li class="list-group-item">Full function coverage</li>
+                    <li class="list-group-item">20" x 30" enlargement with frame</li>
+                    <li class="list-group-item">100 thank you cards</li>
+                    <li class="list-group-item">12" x 30" Fine art or Magazine type photo album</li>
+                    <li class="list-group-item">Seperate family album of the function</li>
+                    <li class="list-group-item fw-semibold">LKR 125 000</li>
+                  </ul>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-outline-dark">Get this package</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-12 mb-4">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="gallery3/10.jpg" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Homecoming Package 01</h5>
+                <p class="card-text">
+                  
+                </p>
+                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal4">
+                    See More
+                  </button>
+              </div>
+            </div>
+          </div>
+          <div class="modal fade" id="myModal4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Homecoming Package 01</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Photo session with couple on desired location</li>
+                    <li class="list-group-item">Full function coverage</li>
+                    <li class="list-group-item">20" x 30" enlargement with frame</li>
+                    <li class="list-group-item">100 thank you cards</li>
+                    <li class="list-group-item fw-semibold">LKR 50 000</li>
+                  </ul>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-outline-dark">Get this package</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="gallery3/9.jpg" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Homecoming Package 02
+
+                </h5>
+                <p class="card-text">
+                      
+                </p>
+                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal5">
+                    See More
+                  </button>
+              </div>
+            </div>
+          </div>
+          <div class="modal fade" id="myModal5" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Homecoming Package 02</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Photo session with couple on desired location</li>
+                    <li class="list-group-item">Full function coverage</li>
+                    <li class="list-group-item">20' x 30' enlargement with frame</li>
+                    <li class="list-group-item">100 thank you cards</li>
+                    <li class="list-group-item">12" x 30" Fine art or Magazine type photo album (40 pages)</li>
+                    <li class="list-group-item fw-semibold">LKR 80 000</li>
                   </ul>
                 </div>
                 <div class="modal-footer">
@@ -130,58 +268,40 @@
                 <p class="card-text">
                   
                 </p>
-                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal1">
+                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal3">
                     See More
                   </button>
               </div>
             </div>
           </div>
         </div>
-
-        <div class="row">
-          <div class="col-lg-4 col-md-12 mb-4">
-            <div class="card">
-              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                <img src="gallery3/10.jpg" class="img-fluid" />
-                <a href="#!">
-                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">Homecoming Package 01</h5>
-                <p class="card-text">
-                  
-                </p>
-                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal1">
-                    See More
-                  </button>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card">
-              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                <img src="gallery3/9.jpg" class="img-fluid" />
-                <a href="#!">
-                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">Homecoming Package 02
-
-                </h5>
-                <p class="card-text">
-                      
-                </p>
-                <button type="button" class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#myModal1">
-                    See More
-                  </button>
+        <div class="modal fade" id="myModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Wedding Package 03</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Photo session with couple on desired location</li>
+                    <li class="list-group-item">Full function coverage</li>
+                    <li class="list-group-item">20" x 30" enlargement with frame</li>
+                    <li class="list-group-item">100 thank you cards</li>
+                    <li class="list-group-item">12" x 30" Fine art or Magazine type photo album</li>
+                    <li class="list-group-item">Seperate family album of the function</li>
+                    <li class="list-group-item fw-semibold">LKR 125 000</li>
+                  </ul>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-outline-dark">Get this package</button>
+                </div>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 mb-4">
+       <!--  <div class="col-lg-4 col-md-6 mb-4">
             <div class="card">
               <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
                 <img src="gallery3/5.jpg" class="img-fluid" />
@@ -200,8 +320,8 @@
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </div>*/
+      </section> -->
       <!--Section: Content-->
 
       <!-- Pagination -->
